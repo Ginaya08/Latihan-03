00d8f88a047365d7b41fa8c8b8642a7c56455b4d
Ginaya08 <55224517+Ginaya08@users.noreply.github.com>
2019-09-19 15:29:33 +0800

    Add files via upload

---
+++ Latihan_03.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	cout << "A or B" << endl;
+	cout << "0 || 0 = " << (0 || 0) << endl;
+	cout << "0 || 0 = " << (0 || 0) << endl;
+	cout << "0 || 1 = " << (0 || 1) << endl;
+	cout << "0 || 1 = " << (0 || 1) << endl;
+	cout << "1 || 0 = " << (1 || 0) << endl;
+	cout << "1 || 0 = " << (1 || 0) << endl;
+	cout << "1 || 1 = " << (1 || 1) << endl;
+	cout << "1 || 1 = " << (1 || 1) << endl;
+	cout << endl << endl;
+	
+	cout << "A or C" << endl;
+	cout << "0 || 0 = " << (0 || 0) << endl;
+	cout << "0 || 1 = " << (0 || 1) << endl;
+	cout << "0 || 0 = " << (0 || 0) << endl;
+	cout << "0 || 1 = " << (0 || 1) << endl;
+	cout << "1 || 0 = " << (1 || 0) << endl;
+	cout << "1 || 1 = " << (1 || 1) << endl;
+	cout << "1 || 0 = " << (1 || 0) << endl;
+	cout << "1 || 1 = " << (1 || 1) << endl;
+	cout << endl << endl;
+	
+	cout << "F2 = (A or B) and (A or C)" << endl;
+	cout << "0 || 0 && 0 || 0 = " << (0 || 0 && 0 || 0) << endl;
+	cout << "0 || 0 && 0 || 1 = " << (0 || 0 && 0 || 1) << endl;
+	cout << "0 || 1 && 1 || 0 = " << (0 || 1 && 0 || 0) << endl;
+	cout << "0 || 1 && 1 || 1 = " << (0 || 1 && 0 || 1) << endl;
+	cout << "1 || 0 && 0 || 0 = " << (1 || 0 && 1 || 0) << endl;
+	cout << "1 || 0 && 0 || 1 = " << (1 || 0 && 1 || 1) << endl;
+	cout << "1 || 1 && 1 || 0 = " << (1 || 1 && 1 || 0) << endl;
+	cout << "1 || 1 && 1 || 1 = " << (1 || 1 && 1 || 1) << endl;
+	
+	return 0;
+}
